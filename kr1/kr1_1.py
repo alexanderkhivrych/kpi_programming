3fc06030bfed5483beb1d67c8a5408aed8301762
alexander.khivrych <alexandr.khivrich@gmail.com>
2019-01-11 12:48:14 +0200

    fixex

--- kr1/kr1_1.py
+++ kr1/kr1_1.py
@@ -1,12 +1,13 @@
 import math
 
-x = float(input("Please enter x: "))
-y = float(input("Please enter y: "))
-
 try:
+    x = float(input("Please enter x: "))
+    y = float(input("Please enter y: "))
+
     R = pow(x, 2) + (0.5 * y + 4.8) / math.sin(x)
     print(f'R={str(R)}')
 
-except ZeroDivisionError as error:
+except ZeroDivisionError:
     print("cant / on 0")
-2
+except ValueError:
+    print("Value must be a natural number!")
