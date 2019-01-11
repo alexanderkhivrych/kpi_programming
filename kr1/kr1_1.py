c02a0fa3cfe056027e9dd376990dde4f004098a6
alexander.khivrych <alexandr.khivrich@gmail.com>
2019-01-11 12:47:26 +0200

    fixex

--- kr1/kr1_1.py
+++ kr1/kr1_1.py
@@ -3,6 +3,10 @@ import math
 x = float(input("Please enter x: "))
 y = float(input("Please enter y: "))
 
-R = pow(x, 2) + (0.5 * y + 4.8) / math.sin(x)
+try:
+    R = pow(x, 2) + (0.5 * y + 4.8) / math.sin(x)
+    print(f'R={str(R)}')
 
-print(f'R={str(R)}')
+except ZeroDivisionError as error:
+    print("cant / on 0")
+2
